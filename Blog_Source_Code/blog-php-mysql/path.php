d6864d9478470e230d3345ff9e91c1b29e29ecd9
akansha <akansha.2000.1602@gmail.com>
2023-04-17 18:04:52 +0530

    my first blog project

---
+++ Blog_Source_Code/blog-php-mysql/path.php
@@ -0,0 +1,7 @@
+<?php
+
+define("ROOT_PATH", realpath(dirname(__FILE__)));
+//define("BASE_URL", "http://localhost:8888/blog");
+#define("BASE_URL","file:///Applications/XAMPP/xamppfiles/htdocs/Blog%20Source%20Code/blog-html-css/index.html");
+define("BASE_URL","file:///Applications/XAMPP/xamppfiles/htdocs/Blog%20Source%20Code/blog-html-css/index.html")
+?>
